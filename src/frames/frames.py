ec28858046fb518219c247e3c15d3e173cf90cbb
klxu03 <beaver2025@gmail.com>
2024-04-19 14:52:16 -0400

    more tests

--- src/frames/frames.py
+++ src/frames/frames.py
@@ -1,9 +1,10 @@
 from .module import RoundSplitter
 import os
 
-if __name__ == "__main__":
+def main():
     splitter = RoundSplitter("1_39.jpg")
     dir_list = os.listdir("/scratch/kxu39")
     print(dir_list)
     print(dir_list[0])
 
+main()
