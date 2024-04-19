b31a84be56128aaa04d5d5d953c38311224eb109
klxu03 <beaver2025@gmail.com>
2024-04-19 14:30:20 -0400

    bring roundsplitters in

--- src/frames/frames.py
+++ src/frames/frames.py
@@ -1,8 +1,8 @@
-# from .module import RoundSplitter
+from .module import RoundSplitter
 import os
 
 if __name__ == "__main__":
-    # splitter = RoundSplitter("1_39.jpg")
+    splitter = RoundSplitter("1_39.jpg")
     dir_list = os.listdir("/scratch/kxu39")
     print(dir_list)
     print(dir_list[0])
