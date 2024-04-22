1e94246b14a28692be540e815c437703a3281697
klxu03 <beaver2025@gmail.com>
2024-04-22 16:09:33 -0400

    threading for main

--- src/captions/main.py
+++ src/captions/main.py
@@ -3,7 +3,25 @@ from load_distributor import load_distribute
 import sys
 import os.path
 import pickle
-from LoggingSubprocess import LoggingSubprocess
+
+import threading
+import queue
+
+q = queue.Queue()
+runner = WhisperXRunner(model_name="large-v3", compute_type="float16", batch_size=16)
+
+def worker():
+    while True:
+        try:
+            item = q.get()
+        except queue.Queue.Empty:
+            break
+
+        print(f'Working on {item}')
+        runner.run(item, item)
+        print(f'Finished {item}')
+
+        q.task_done()
 
 def main():
     index = int(sys.argv[1]) # 0-indexed
@@ -17,10 +35,15 @@ def main():
         print("Run load_distribute, couldn't find pkl")
         videos_to_split = load_distribute(max_index, index)
 
+    threading.Thread(target=worker, daemon=True).start()
+
     print("videos_to_split", videos_to_split)
     for video in videos_to_split:
-        command = f"poetry run python WhisperXRunner.py {video}"
-        LoggingSubprocess(command, shell=False).start()
+        q.put(f"{video}/")
+
+    q.join()
+    print("Finished index", index)
+
 
 if __name__ == "__main__":
     main()
