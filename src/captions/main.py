93a7ba3cf5c275e50664bdda60aba7c94e41ee56
klxu03 <beaver2025@gmail.com>
2024-04-21 21:50:52 -0400

    run whisperx captioning

---
+++ src/captions/main.py
@@ -0,0 +1,16 @@
+from WhisperXRunner import WhisperXRunner
+from load_distributor import load_distribute
+import sys
+
+def main():
+    index = int(sys.argv[1]) # 0-indexed
+    max_index = int(sys.argv[2])
+    videos_to_split = load_distribute(max_index, index)
+
+    print("videos_to_split", videos_to_split)
+    for video in videos_to_split:
+        runner = WhisperXRunner(f"{video}/", f"{video}/", model_name="large-v3", compute_type="float16", batch_size=16)
+        runner.run()
+
+if __name__ == "__main__":
+    main()
