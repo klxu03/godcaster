cf22e6fbffbff8baec61cba883f8402b38d07c7d
Kuleen Sasse <cometku@gmail.com>
2024-04-21 23:28:01 -0400

    Added previous text transformers and video encoder

--- src/model/godcaster.py
+++ src/model/godcaster.py
@@ -6,6 +6,8 @@ import torch
 
 import torch.nn as nn
 
+from transformers import VivitImageProcessor, VivitModel, LongformerModel
+
 from godcaster_encoder import GodCasterEncoder
 from trainer.godcaster_trainer import GodCasterConfig
 
@@ -83,6 +85,12 @@ class GodCaster(nn.Module):
                                               nn.Tanh(), nn.Linear(config.hidden_size, config.hidden_size))
     
         self.input_transformers = GodCasterEncoder(config)
+
+        self.vivit_processor = VivitImageProcessor()
+
+        self.vivit_model = VivitModel(config.vivit_config)
+
+        self.context_transformer = LongformerModel(config.text_config)
 
         self.register_buffer("position_ids", torch.arange(config.max_position_embeddings).expand((1, -1)))
         self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
@@ -168,7 +176,14 @@ class GodCaster(nn.Module):
         emb_inputs = self.position_embeddings(position_ids) + emb_x + emb.unsqueeze(1).expand(-1, seq_length, -1)
         emb_inputs = self.dropout(self.LayerNorm(emb_inputs))
 
-        input_trans_hidden_states = self.input_transformers(emb_inputs, video, text).last_hidden_state
+        text_emb = self.context_transformer(text)
+
+        video_emb = self.vivit_processor(video)
+
+        video_emb = self.vivit_model(video_emb)
+        
+
+        input_trans_hidden_states = self.input_transformers(emb_inputs, video_emb, text_emb).last_hidden_state
 
         h = self.output_down_proj(input_trans_hidden_states)
         
